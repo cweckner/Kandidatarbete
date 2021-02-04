644617c930cc9f7567eab554135cf6c8faa308a1
Nora Ojensa <ojensa@student.chalmers.se>
2021-02-04 17:38:18 +0100

    Omstrukturerat & lagt in mer grejer på inputscreen

--- Display/main.py
+++ Display/main.py
@@ -3,24 +3,23 @@ from screen_nav import sc_helper
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivymd.uix.button import MDRectangleFlatButton
+from kivymd.uix.picker import MDTimePicker, MDDatePicker
 
 
 
 class WelcomeScreen(Screen):
     pass
 
-class LoginScreen(Screen):
-    pass
-
 class InputScreen(Screen):
     pass
 
-
+class GoodbyeScreen(Screen):
+    pass
 
 screen_manager = ScreenManager()
 screen_manager.add_widget(WelcomeScreen(name = 'welcome'))
-screen_manager.add_widget(LoginScreen(name = 'login'))
 screen_manager.add_widget(InputScreen(name = 'inputs'))
+screen_manager.add_widget(GoodbyeScreen(name = 'goodbye'))
 
 
 class DemoApp(MDApp):
@@ -29,6 +28,21 @@ class DemoApp(MDApp):
         self.theme_cls.primary_palette = "Green"
         screen = Builder.load_string(sc_helper)  
         return screen
+
+    def show_time_picker(self):
+        '''Open time picker dialog.'''
+
+        time_dialog = MDTimePicker()
+        time_dialog.open()
+
+    def get_date(self, date):
+        '''
+        :type date: <class 'datetime.date'>
+        '''
+
+    def show_date_picker(self):
+        date_dialog = MDDatePicker(callback=self.get_date)
+        date_dialog.open()
 
 #if self.username.text == "admin":
 #           root.manager.current = 'inputs'
--- Display/screen_nav.py
+++ Display/screen_nav.py
@@ -4,8 +4,8 @@ sc_helper = """
 
 ScreenManager:
     WelcomeScreen:
-    LoginScreen:
     InputScreen:
+    GoodbyeScreen:
 
 
 <WelcomeScreen>:
@@ -14,34 +14,71 @@ ScreenManager:
         text: 'Welcome, please scan your RFID-tag'
         halign: 'center'
     MDRectangleFlatButton:
-        text: 'or log in manually'
-        pos_hint: {'center_x': 0.5, 'center_y': 0.4}
-        on_press: root.manager.current = 'login'
-
-
-<LoginScreen>:
-    name: 'login'
-    MDTextField:
-        hint_text: "Enter username"
-        helper_text: "or click on forgot username"
-        helper_text_mode: "on_focus"
-        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
-        size_hint_x: None
-        width: 300
-    MDRectangleFlatButton:
-        text: 'Go directly to input screen'
+        text: 'Go directly to input screen (For test purposes only)'
         pos_hint: {'center_x': 0.5, 'center_y': 0.4}
         on_press: root.manager.current = 'inputs'
-        
+
 
 <InputScreen>:
     name: 'inputs'
+
+    MDLabel:
+        text: 'Insert User Input'
+        halign: 'center'
+
     MDTextField:
-        hint_text: "Current battery level"
+        hint_text: "Current battery level %"
         mode: "rectangle"
         size_hint_x: 0.5
-        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+        pos_hint: {'center_x': 0.3, 'center_y': 0.75}
 
+    MDTextField:
+        hint_text: "Wanted charge level at departure %"
+        mode: "rectangle"
+        size_hint_x: 0.5
+        pos_hint: {'center_x': 0.3, 'center_y': 0.65}
+
+    MDTextField:
+        hint_text: "Battery Capacity"
+        mode: "rectangle"
+        size_hint_x: 0.5
+        pos_hint: {'center_x': 0.3, 'center_y': 0.55}
+
+    MDTextField:
+        hint_text: "Max current of EV battery"
+        mode: "rectangle"
+        size_hint_x: 0.5
+        pos_hint: {'center_x': 0.3, 'center_y': 0.45}
+
+    MDLabel:
+        text: "Departure Date & Time"
+        pos_hint: {'center_x': 0.6, 'center_y': 0.35}
+
+    MDRaisedButton:
+        text: "Open time picker"
+        pos_hint: {'center_x': 0.4, 'center_y': 0.35}
+        on_release: app.show_time_picker()
+
+    MDRaisedButton:
+        text: "Open date picker"
+        pos_hint: {'center_x': 0.6, 'center_y': 0.35}
+        on_release: app.show_date_picker()
+
+    MDRectangleFlatButton:
+        text: 'Go directly to goodbye screen'
+        pos_hint: {'center_x': 0.5, 'center_y': 0.1}
+        on_press: root.manager.current = 'goodbye'
+
+
+<GoodbyeScreen>:
+    name: 'goodbye'
+    MDLabel:
+        text: 'Thank you, your car will now charge optimally'
+        halign: 'center'
+    MDRectangleFlatButton:
+        text: 'Go back'
+        pos_hint: {'center_x': 0.5, 'center_y': 0.1}
+        on_press: root.manager.current = 'inputs'
     
 """
 
