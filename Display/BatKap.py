f6fe6f338d21ae9cfe8fd3297b6b854c7499c332
KalleSvantesson <77684922+KalleSvantesson@users.noreply.github.com>
2021-02-16 14:55:04 +0100

    Update BatKap.py

--- Display/BatKap.py
+++ Display/BatKap.py
@@ -2,12 +2,12 @@ import csv
 
 Brand = input("Car Brand?")
 Model = input("Car Model?")
+def CARSPEC(b,m):
+    with open('Bilkap.csv','r') as infile:
+        reader = csv.reader(infile, delimiter=",")
+        for row in reader:
+            if b == row[0]:
+                if m == row[1]:
+                    return "Battery Capasity" + "\t" + row[2] + "\n" + "ChargePower" + "\t" + row[3]
 
-with open('Bilkap.csv','r') as infile:
-    reader = csv.reader(infile, delimiter=",")
-    for row in reader:
-        if Brand == row[0]:
-            if Model == row[1]:
-                print("Battery Capasity" + "\t" + row[2] + "\n" + "ChargePower" + "\t" + row[3])
-                break
-
+print(CARSPEC(Brand,Model))
