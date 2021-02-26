2450335a77459f9a75c5c21d76d0203db93633f9
williamtruve <74651953+williamtruve@users.noreply.github.com>
2021-02-26 15:08:43 +0100

    najs

--- Microcontroller/optimisation.py
+++ Microcontroller/optimisation.py
@@ -1,57 +1,16 @@
-########### Python 2.7 #############
-import httplib, urllib, base64
+#Meteo hourly data
+#Site, Solcast_site_11.979359_57.686904
+#Country, Sweden
+#Data Source, Solcast https://solcast.com
+#Time step, 1
+#Latitude, 57.6869
+#Longitude, 11.9794
+#Altitude, 60
+#Time Zone, 0.0
+#Year,Month,Day,Hour,Minute,GHI
+#Year,Month,Day,Hour,Minute,[W/m2]
+import csv
 
-headers = {
-    # Request headers
-    'Accept-Encoding': '',
-    'Ocp-Apim-Subscription-Key': '{subscription key}',
-    'Authorization': '{access token}',
-}
 
-params = urllib.urlencode({
-    # Request parameters
-    'deliveryarea': '{string}',
-    'startTime': '{string}',
-    'endTime': '{string}',
-})
-
-try:
-    conn = httplib.HTTPSConnection('marketdata-api.nordpoolgroup.com')
-    conn.request("GET", "/operatingdata/hydroReservoir/content?%s" % params, "{body}", headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
-
-####################################
-
-########### Python 3.2 #############
-import http.client, urllib.request, urllib.parse, urllib.error, base64
-
-headers = {
-    # Request headers
-    'Accept-Encoding': '',
-    'Ocp-Apim-Subscription-Key': '{subscription key}',
-    'Authorization': '{access token}',
-}
-
-params = urllib.parse.urlencode({
-    # Request parameters
-    'deliveryarea': '{string}',
-    'startTime': '{string}',
-    'endTime': '{string}',
-})
-
-try:
-    conn = http.client.HTTPSConnection('marketdata-api.nordpoolgroup.com')
-    conn.request("GET", "/operatingdata/hydroReservoir/content?%s" % params, "{body}", headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
-
-####################################
+with open('PVforecast', 'r') as infile:
+reader = csv.reader(infile,delimiter=",")
