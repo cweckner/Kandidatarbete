08a5c471303b7f30f713b6432c893dc16f11e7fe
williamtruve <74651953+williamtruve@users.noreply.github.com>
2021-03-02 13:02:23 +0100

    lite grejor

---
+++ Microcontroller/parameters.py
@@ -0,0 +1,5 @@
+import datetime
+#TODO ta fram dictionary och slå samman alla parametrar för angiven tid
+#TODO fråga living lab om PV data för att jämföra med väder data
+#TODO uppdatera hur mycket batteriet har laddats?
+
