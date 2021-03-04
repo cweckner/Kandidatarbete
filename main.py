45984030659870d1e3ca9aa8cd8b437101ccfa1f
Samuel Jönsson <42651767+Saunto@users.noreply.github.com>
2021-03-04 13:44:57 +0100

    tror charge-loopens struktur är klar

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ print(intervalStart)
 intervalEnd = datetime.datetime.now().strftime('%Y-%m-%d')
 timeSetRFIDtagID = commands.timeConverter(time, "setRFIDtagID")
 commands.requestSiteInfo(token)
-commands.connectorStatus(token)
+commands.connectorStatus(token,outlet)
 timeStop = commands.timeConverter(time,"startCharger")
 commands.startCharger(token,transactionID,tagID,timeStop)
 commands.consumedEnergy(token,tagID,intervalStart,intervalEnd)
@@ -39,32 +39,44 @@ for length of array of optimised charging
 '''
 '%Y-%m-%d %H:%M:%S'
 #commands.timeConverter("2020-02-26 14:47:20","consumedEnergy")
-'''
+
 #Delay for 5 minutes
 def delay():
     sched.scheduler(time.time(), time.sleep(300))
-
 
 
 screen = disp.DemoApp().run()
 transactionID= "00000000-0000-0000-0000-000000000000"
 tagID = "918273645"
 
-while(screen.start == 1):
+while(screen.readytosend == 1):
+    #Initial inputs needed to continue
     token = commands.createToken()
     currentTransactionID = commands.incrementTransactionID(transactionID)
-    commands.startCharger(token,currentTransactionID,tagID)
+
     timeString = str(screen.datepicker) + str(screen.timepicker)
     endTimeCharging = datetime.datetime.strptime(timeString, '%Y-%m-%d %H:%M:%S')
     numberOfUpdates = commands.calculateNumberOfUpdates(endTimeCharging)
+    outlet = screen.outletcbx
+    timeNow = datetime.datetime.strptime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+    startDate = commands.timeConverter(timeNow, "consumedEnergy")
+    endDate = commands.timeConverter(endTimeCharging, "consumedEnergy")
 
+    stopTime = commands.timeConverter(endTimeCharging, "startCharger")
+    commands.startCharger(token, currentTransactionID, tagID, stopTime)
     
+    #This will be the loop that runs the whole charging process
     while(numberOfUpdates > 0):
         chargingCurrent = optireal.current(endTimeCharging, screen.maxcurrenttf, screen.batterytf, screen.wantedtf, screen.currenttf)
-
-
-        numberOfUpdates -= 1
-'''
-token = commands.createToken()
-commands.connectorStatus(token,1)
-commands.connectorStatus(token,2)
+        outletStatus = commands.connectorStatus(token, outlet)
+        if(outletStatus != "AVAILABLE"):
+            commands.changeActiveCurrent(token, outlet, chargingCurrent)
+            numberOfUpdates -= 1
+            delay()
+        else:
+            #if the outlet is available -> car is not connected to the charger -> stop the charging loop
+            break
+    
+    #All iterations done, stop the charger
+    commands.consumedEnergy(token, tagID, startDate, endDate)
+    commands.stopCharger(token, currentTransactionID)
