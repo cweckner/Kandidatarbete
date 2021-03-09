d0efaa9554a6e0020a4f45a6098d1b0753be55bb
Samuel Jönsson <42651767+Saunto@users.noreply.github.com>
2021-03-09 16:14:46 +0100

    choose between using delay or comparing time deltas

--- Microcontroller/backend.py
+++ Microcontroller/backend.py
@@ -30,6 +30,7 @@ class backend:
                 numberOfUpdates = commands.calculateNumberOfUpdates(departureTime)
                 chargingOutlet = outlet
                 otherOutlet = 3
+                nexTime = datetime.datetime.now()
                 voltage = 400 #Charger specific voltage
 
                 #time format conversion for the consumedEnergy command to see how much energy it took to charge the car
@@ -47,7 +48,9 @@ class backend:
                 while(numberOfUpdates > 0):
                     #check if a car is connected to the outlet we want to use
                     outletStatus = commands.connectorStatus(token, chargingOutlet)
-                    if(outletStatus != "AVAILABLE"):
+                    #update currentTime to the time right now
+                    currentTime = datetime.datetime.now()
+                    if(outletStatus != "AVAILABLE" and currentTime >= nexTime):
                         #receiving the optimised charging current from the optimisation model
                         chargingCurrent = optireal.current(departureTime, int(carMaxCurrentInput), int(carBatteryCapacity), int(carWantedCharge), int(carChargeLevelNow+0.5), currentTime)
 
@@ -61,15 +64,18 @@ class backend:
                         print(str(int(carChargeLevelNow+0.5)) + "%")
 
                         #update what time it is, only for simulation
-                        currentTime += datetime.timedelta(minutes = 5)
-                        print(currentTime)
+                        #currentTime += datetime.timedelta(minutes = 5)
+                        #print(currentTime)
+
+                        #instead of using delay function check if 5 minutes have passed
+                        nextTime = currentTime + datetime.timedelta(minutes = 5)
 
                         numberOfUpdates -= 1
-                        self.delay()
+                        #self.delay()
                     #if the outlet is available -> no car is connected to the outlet -> stop the charging
-                    else:
+                    elif(outletStatus == "AVAILABLE"):
                         break
-                
+                        
                 #All iterations done, or the car is not connected and the other outlet is not in use -> stop the charger
                 if(outlet == 1):
                     otherOutlet = 2
