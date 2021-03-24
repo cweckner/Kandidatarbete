35cdc1c149b5393e6a2f8ee47f8c63dcc3619ee8
williamtruve <74651953+williamtruve@users.noreply.github.com>
2021-03-24 15:04:34 +0100

    uppdatering till 24h

--- Microcontroller/parameters.py
+++ Microcontroller/parameters.py
@@ -2,45 +2,19 @@ import datetime
 #TODO ta fram dictionary och slå samman alla parametrar för angiven tid
 #TODO fråga living lab om PV data för att jämföra med väder data
 #TODO uppdatera hur mycket batteriet har laddats?
-priser = {
-    0: 67,
-    1: 65,
-    2: 50,
-    3: 45,
-    4: 46,
-    5: 45,
-    6: 58,
-    7: 68,
-    8: 44,
-    9: 67,  # Dessa ska beräknas senare
-    10: 80,
-    11: 43,
-    12: 50,
-    13: 86,
-    14: 79,
-    15: 30,
-    16: 87,
-    17: 86,
-    18: 95,
-    19: 87,
-    20: 85,
-    21: 78,
-    22: 74,
-    23: 72,
-}   #Nyckel = datum + H, värde = pris/load/sol Gör 3 separata till att börja med
+# #Nyckel = datum + H, värde = pris/load/sol Gör 3 separata till att börja med
+year = 2021
 mon = 1
 day = 1
 hou = 0
 tidspris = {
 }
-for j in range(3 * 24 + 1):
-    tidspris[datetime.datetime(2021, mon, day, hou, 0, 0)] =  60
-    hou = (hou + 1) % 24
-    if hou == 0:
-        day = day + 1
-    if day == 30:
-        mon = mon + 1
-
+date = datetime.datetime(year, mon, day, hou)
+for j in range(365 * 24 + 1):
+    #print (date)
+    tidspris[date] =  60                                #Skapar en dictionary med alla datum/timslag som nycklar för år 2021
+    date = date + datetime.timedelta(hours=1)
+                                                        #Värden kan dock uppdateras varje gång om man tänker forecast
 def param(time_now,end_time):
     #Om end_time - time_now är större än 12h skickar vi 12 värden tillbaka
     #Om end_time - time_now är mindre än 12h skickar vi återstående värden tillbaka
@@ -50,25 +24,32 @@ def param(time_now,end_time):
     i = int((((end_time-time_now).total_seconds())/3600)+0.5)   #laddningstid avrundat till timmar
     #print(i)
     #print("antal timmar avrundat")
-    y = int((time_now+datetime.timedelta(minutes = 30)).strftime("%H"))
+    z = int((time_now+datetime.timedelta(minutes = 0)).strftime("%H"))
+    y = int((time_now+datetime.timedelta(minutes = 30)).strftime("%H")) #TODO: Kolla efter buggar vid 23:30 tider
 
     #print(y)
     #print("vilken timme som är nu")
-    if i < 12:
+    if z == 23 and y == 0:
+        p = datetime.datetime(time_now.year, time_now.month, time_now.day + 1, y)
+    else:
+        p = datetime.datetime(time_now.year, time_now.month, time_now.day, y)
+    if i < 24:
         for j in range(y,y+i+1):
             #print(j)
-            p = j % 24
-            params[p] = priser[p]
+           #print(p)
+            params[p] = tidspris[p]
+            p = p + datetime.timedelta(hours=1)
     else:
-        i = 12
+        i = 24
         for j in range(y,y+i+1):
-            p = j % 24
-            params[p] = priser[p]
-    #print(params)
-    #print("retur från params")
+            #print(p)
+            params[p] = tidspris[p]
+            p = p + datetime.timedelta(hours=1)
+    print(params)
+
     return params
 
-#end_time = datetime.datetime(2021,3,3,8,0,0)
+#end_time = datetime.datetime(2021,3,27,8,0,0)
 #param(datetime.datetime.now(),end_time)
 #print(param(datetime.datetime.now(),end_time))
 
