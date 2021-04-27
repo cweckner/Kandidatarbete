d7d689727bbaead2cb7a58f7e3454958a527d20b
Crippz <weckner@student.chalmers.se>
2021-04-27 14:58:54 +0200

    skjut mig

---
+++ end.py
@@ -0,0 +1,4 @@
+import commands
+
+t = commands.createToken()
+commands.stopCharger(t,"00000000-0000-0000-0000-000000001002" ,1)
---
+++ rf.py
@@ -0,0 +1,5 @@
+import rebase_forecast
+
+fc = rebase_forecast.get_solar_forecast("16:24")
+
+print(fc)
