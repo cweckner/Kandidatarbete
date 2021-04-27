a5b26bf7569fd915840430d58dd1628b3e72bf12
Crippz <weckner@student.chalmers.se>
2021-04-27 14:53:21 +0200

    testscript eller nåe

--- test_script2.py
+++ test_script2.py
@@ -2,16 +2,28 @@ import commands
 import datetime
 import time
 
-transactID = str("00000000-0000-1110-0000-000000000000")
+
 token = commands.createToken()
-commands.connectorStatus(token, 1)
 
-commands.enableCharger(token, transactID, "1234", 1)
-commands.startCharger(token, transactID,"1234",1)
+transactID = str("00000000-0000-0000-0000-000000000300")
+print(commands.connectorStatus(token, 1))
+time.sleep(10)
+print(1)
+commands.enableCharger(token, transactID, "12345", 1)
+print(commands.connectorStatus(token, 1))
+time.sleep(10)
+print(2)
+commands.startCharger(token, transactID,"12345",1)
+print(commands.connectorStatus(token, 1))
 
-commands.changeActiveCurrent(token, "1", "8")
+time.sleep(10)
+print(3)
+commands.changeActiveCurrent(token, "1", "6")
+print(commands.connectorStatus(token, 1))
 
-chargeLog = commands.consumedEnergy(token, "1234", "2021-04-27", "2021-04-27")
+time.sleep(10)
+
+chargeLog = commands.consumedEnergy(token, "12345", "2021-04-27", "2021-04-27")
 
 timeprint = datetime.datetime.now()
 
@@ -20,9 +32,9 @@ f.write(str(timeprint))
 f.write(chargeLog + "\n")
 f.close()
 
-time.sleep(300)
+time.sleep(25)
 
-chargeLog2 = commands.consumedEnergy(token, "1234", "2021-04-27", "2021-04-27")
+chargeLog2 = commands.consumedEnergy(token, "12345", "2021-04-27", "2021-04-27")
 
 timeprint2 = datetime.datetime.now()
 
@@ -30,5 +42,8 @@ f = open("chargingLogTest.txt", "a")
 f.write(str(timeprint2))
 f.write(chargeLog2 + "\n")
 f.close()
-
 commands.stopCharger(token, transactID, 1)
+
+
+time.sleep(10)
+print(commands.connectorStatus(token, 1))
